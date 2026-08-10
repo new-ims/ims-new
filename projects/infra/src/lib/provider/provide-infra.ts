05a23416c91312c0506467bb57445f24eadd9be2
Kobi Hari <kobi2294@yahoo.com>
2026-08-10 16:05:21 +0300

    feat: update provideInfra function to accept InfraOptions for improved API service integration

--- projects/infra/src/lib/provider/provide-infra.ts
+++ projects/infra/src/lib/provider/provide-infra.ts
@@ -1,11 +1,19 @@
 import { EnvironmentProviders, inject, makeEnvironmentProviders, provideAppInitializer } from "@angular/core";
 import { BootstrapService } from "../services/bootstrap.service";
+import { ApiServiceBase } from "../services/injected/api";
+import type { Type } from "@angular/core";
+import { provideApi } from "./provide-api";
 
-export function provideInfra(): EnvironmentProviders {
+export interface InfraOptions {
+    readonly apiService: Type<ApiServiceBase>
+}
+
+export function provideInfra(options: InfraOptions): EnvironmentProviders {
     return makeEnvironmentProviders([
         provideAppInitializer(async () => {
             const bootstrapper = inject(BootstrapService);
             await bootstrapper.start();
-        })
+        }), 
+        provideApi(options.apiService)
     ]);
 }
--- projects/process-app/src/app/app.config.ts
+++ projects/process-app/src/app/app.config.ts
@@ -1,11 +1,13 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideApi, provideInfra } from '@infra';
 import { ApiService } from './services/api.service';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(), 
-    provideInfra(), 
-    provideApi(ApiService)
+    provideInfra({
+      apiService: ApiService
+    }),
   ],
 };
