e9ba255d801791144ebd21e8edb3a396945f25bd
Kobi Hari <kobi2294@yahoo.com>
2026-08-10 16:05:50 +0300

    refactor: remove unused provideApi import from app configuration

--- projects/process-app/src/app/app.config.ts
+++ projects/process-app/src/app/app.config.ts
@@ -1,7 +1,6 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideApi, provideInfra } from '@infra';
+import { provideInfra } from '@infra';
 import { ApiService } from './services/api.service';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
