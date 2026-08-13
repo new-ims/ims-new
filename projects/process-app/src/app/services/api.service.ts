fa95144a755f5d3bcc382adf9f39262ebdad4a17
Kobi Hari <kobi2294@yahoo.com>
2026-08-13 13:25:52 +0300

    feat: add delay utility function and update ApiService to use it for simulating network delay

--- projects/process-app/src/app/services/api.service.ts
+++ projects/process-app/src/app/services/api.service.ts
@@ -2,12 +2,14 @@ import { Service } from "@angular/core";
 import { ApiServiceBase } from "@infra";
 import { FakeModels } from "@fake-models";
 import { Api } from "@common/api";
+import { delay } from "@common/utils";
 
 
 @Service() 
 export class ApiService implements ApiServiceBase<FakeModels.FakeProcesses> {
     async login(input: Api.LoginInput): Promise<Api.LoginOutput<FakeModels.FakeProcesses>> {
-        console.log("ApiService.externalLogin called with input:", input);
+        await delay(1000); // Simulate network delay
+        console.log("ApiService.externalLogin called with input:", input.params);
         return {
             processKey: '501275', 
             processType: 'holiday'
