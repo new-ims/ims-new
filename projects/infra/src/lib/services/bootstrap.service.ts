2e70edeb7bb1c4e517060fc142ed9d0685c85a31
Kobi Hari <kobi2294@yahoo.com>
2026-08-13 15:35:03 +0300

    feat: refactor BootstrapService to improve login method structure and encapsulate API injection

--- projects/infra/src/lib/services/bootstrap.service.ts
+++ projects/infra/src/lib/services/bootstrap.service.ts
@@ -1,15 +1,23 @@
-import { inject, Service } from "@angular/core";
-import { API_SERVICE_TOKEN } from "./injected/api";
-import { getUrlParams } from "@common/utils";
+import { inject, Service } from '@angular/core';
+import { API_SERVICE_TOKEN } from './injected/api';
+import { getUrlParams } from '@common/utils';
 
 @Service()
 export class BootstrapService {
-    async start() {
-        const api = inject(API_SERVICE_TOKEN);
-        const params = getUrlParams();
-        const login = await api.login({
-            params
-        });
-        console.log("BootstrapService.start: externalLogin result:", login);        
-    }
+  readonly #api = inject(API_SERVICE_TOKEN);
+
+
+  async start() {
+    const loginOutput = await this.login();
+
+
+  }
+
+  async login() {
+    const params = getUrlParams();
+    const login = await this.#api.login({
+      params,
+    });
+    return login;
+  }
 }
