3bff1ff78f89fbd3426f5ee82a07690465b7588e
Kobi Hari <kobi2294@yahoo.com>
2026-08-13 14:31:46 +0300

    feat: update core types and add MOCK_PROCESSES mock data for testing

--- projects/common/models/core.types.ts
+++ projects/common/models/core.types.ts
@@ -1,10 +1,14 @@
 export interface BaseProcess<T extends string = string> {
-    readonly processType: T;
+  readonly processType: T;
+  readonly processKey: string;
 }
 
 export type ProcessMapper = BaseProcess[];
 export type ProcessTypeKeys<MAPPER extends ProcessMapper> = MAPPER[number]['processType'];
 
-export type ProcessOf<MAPPER extends ProcessMapper, Key extends ProcessTypeKeys<MAPPER>> = Extract<MAPPER[number], { processType: Key }>;
+export type ProcessOf<MAPPER extends ProcessMapper, Key extends ProcessTypeKeys<MAPPER>> = Extract<
+  MAPPER[number],
+  { processType: Key }
+>;
 
 export type ProcessType<T extends BaseProcess> = T['processType'];
--- projects/process-app/src/mock/processes.ts
+++ projects/process-app/src/mock/processes.ts
@@ -1,5 +1,34 @@
-import { FakeModels } from "@fake-models";
+import { FakeModels } from '@fake-models';
 
-export const MOCK_PROCESSES: FakeModels.FakeProcesses[] = [
-    
+export const MOCK_PROCESSES: FakeModels.FakeProcesses[number][] = [
+  {
+    processType: 'holiday',
+    processKey: 'ho000001',
+    vacationType: 'leisure',
+  },
+  {
+    processType: 'holiday',
+    processKey: 'ho000002',
+    vacationType: 'adventure',
+  },
+  {
+    processType: 'holiday',
+    processKey: 'ho000003',
+    vacationType: 'cultural',
+  },
+  {
+    processType: 'radiant-health',
+    processKey: 'rh000010',
+    degreeOfHealth: 92,
+  },
+  {
+    processType: 'radiant-health',
+    processKey: 'rh000002',
+    degreeOfHealth: 76,
+  },
+  {
+    processType: 'radiant-health',
+    processKey: 'rh000012',
+    degreeOfHealth: 58,
+  },
 ];
