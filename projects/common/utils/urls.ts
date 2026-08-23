a971b5738a3379a13f48990ba2233f9397449df0
Kobi Hari <kobi2294@yahoo.com>
2026-08-23 10:32:58 +0300

    feat: enhance getUrlParams function to include hash parameters and refactor code structure

--- projects/common/utils/urls.ts
+++ projects/common/utils/urls.ts
@@ -1,4 +1,21 @@
 export function getUrlParams(): Record<string, string> {
     const url = new URL(window.location.href);
-    return Object.fromEntries(url.searchParams);
+
+    const searchParams = Object.fromEntries(url.searchParams);
+    const hashParams = getHashParams(url.hash);
+
+    return {
+        ...searchParams,
+        ...hashParams,
+    };
+}
+
+function getHashParams(hash: string): Record<string, string> {
+    const queryStart = hash.indexOf('?');
+    if (queryStart === -1) {
+        return {};
+    }
+
+    const queryString = hash.slice(queryStart + 1);
+    return Object.fromEntries(new URLSearchParams(queryString));
 }
