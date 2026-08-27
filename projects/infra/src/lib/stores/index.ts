fe692b3bb017356cf15bb4cbe37d3be0cd1348f8
Kobi Hari <kobi2294@yahoo.com>
2026-08-27 12:13:54 +0300

    feat: add index file for stores to export configuration and process modules

---
+++ projects/infra/src/lib/stores/index.ts
@@ -0,0 +1,8 @@
+export * from './config/config.slice';
+export * from './config/config.store';
+export * from './config/config.vm';
+export * from './config/config.vm-builder';
+
+export * from './process/process.slice';
+export * from './process/process.store';
+export * from './process/process.injector';
