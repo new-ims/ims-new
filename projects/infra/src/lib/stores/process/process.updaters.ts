30a7a5dd5b2965fc972ebf4fa5bb94d8cb02a656
jennyEventov <e.jenny.y@gmail.com>
2026-09-16 14:22:23 +0300

    feat: remove unused import for InfoVm in process updaters

--- projects/infra/src/lib/stores/process/process.updaters.ts
+++ projects/infra/src/lib/stores/process/process.updaters.ts
@@ -1,6 +1,5 @@
 import { PartialStateUpdater } from "@ngrx/signals";
 import { ProcessSlice } from "./process.slice";
-import { InfoVm } from "./view-models/infos/infos.vm";
 
 export function selectInfo(infoId: string): PartialStateUpdater<ProcessSlice> {
     return _ => ({
